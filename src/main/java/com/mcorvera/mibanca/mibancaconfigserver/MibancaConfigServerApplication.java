f6324b92ffb6bda167316e9d98fb20ff8e7c6b81
mcorversoft <milton.corvera.ing@gmail.com>
2023-02-14 18:27:40 -0600

    first commit

---
+++ src/main/java/com/mcorvera/mibanca/mibancaconfigserver/MibancaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mcorvera.mibanca.mibancaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MibancaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MibancaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcorvera/mibanca/mibancaconfigserver/MibancaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcorvera.mibanca.mibancaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MibancaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
