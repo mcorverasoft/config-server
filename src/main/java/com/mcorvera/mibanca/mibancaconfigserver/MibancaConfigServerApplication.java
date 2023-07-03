6c9ebb07e6dc48a84db912f6b06613f14420907a
mcorversoft <milton.corvera.ing@gmail.com>
2023-07-03 15:10:20 -0600

    update configs

--- src/main/java/com/mcorvera/mibanca/mibancaconfigserver/MibancaConfigServerApplication.java
+++ src/main/java/com/mcorvera/mibanca/mibancaconfigserver/MibancaConfigServerApplication.java
@@ -2,10 +2,11 @@ package com.mcorvera.mibanca.mibancaconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MibancaConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MibancaConfigServerApplication.class, args);
 	}
